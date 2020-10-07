de64d7267272370afb685e4ec440c1924ef5ce68
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-10-07 18:55:22 +1100

    minor UI changes part 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo" onClick={closeMobileMenu} >
-                    KYT
+                    KYT&ensp; <i className="fas fa-trash-restore" />
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : "fas fa-bars"} />
